38074a98b57bcfcf2ea8b9ca56f167f089a5271b
Standard <Standard@Erics-MacBook-Air-352.local>
2019-03-15 17:44:01 -0400

    Implement Queue

--- Arduino/src/test/Mar8Test/Mar8Test.ino
+++ Arduino/src/test/Mar8Test/Mar8Test.ino
@@ -9,10 +9,18 @@
 #define Bottom_Left 7
 #define Bottom_Centre 3
 #define Bottom_Right 8
-#define Hole 26
-#define Crack 27
+
 #define NO 0
 #define YES 1
+
+#define HOLE 10
+#define LEFT_CRACK 11
+#define RIGHT_CRACK 110
+#define CENTRE_CRACK 111
+
+#define Hole 26
+#define Crack 27
+
 #define LEFT 11
 #define RIGHT 12
 #define CENTRE 13
@@ -42,6 +50,26 @@ int hole_or_crack = NA;
 volatile int crack_position = NA; 
 void detection_ISR();
 
+//Initialize global variable queues
+//Queue Code//
+struct Queue {
+    int store[4]; /*the array with the elements*/
+    int max_size;
+    int size;
+    int start;
+};
+typedef struct Queue Queue;
+
+int isEmpty(Queue x); 
+int deq(Queue *x, int *pval);
+int enq(Queue *x, int val);
+int init(Queue *x); 
+
+int r=0;
+Queue HC_q;
+Queue HC_Dist_q; 
+Queue C_Pos_q; 
+
 //Initiate DC Motors
 // AerDCMotors(int pinL1, int pinL2, int pwmL, int pwmR)
 // pinR1 = A4, pinR2 = A5
@@ -59,12 +87,14 @@ void setup() {
   dc.init();
   servo.init(); 
   stepp.init();
-
+  init(&HC_q);
+  
   //Interrupts 
   pinMode(Bottom_Centre, INPUT);
   attachInterrupt(digitalPinToInterrupt(Bottom_Centre), detection_ISR, RISING);
   Serial.begin(9600);
   Serial.print("start");
+  Serial.print("\n");
   Serial.print("\n");
   
 }
@@ -73,11 +103,13 @@ void loop() {
   // Move forward
   dc.forward(255);
   while (1){
-      if (cone_deploy){
+      if (isEmpty(HC_q)){
+        Serial.print("Hey");
         cone_deployment();
 //        dc.forward(255);
         cone_deploy = false; 
-        Serial.print("done deploy");
+        Serial.print("done deployment");
+        Serial.print("\n");
       }
   }
 //  servo.move_to(9.5); 
@@ -87,22 +119,11 @@ void loop() {
 }
 
 //Interrupt sequence if cone detected
-
-     hole = NA;
-     left_crack = NA; 
-     right_crack = NA; 
-     return;
-}
-*/
-
 unsigned long lastInterrupt = 0;
 void detection_ISR(){
 
     // To prevent repeated input due to bad sensor signal
     if (millis() - lastInterrupt > 1) {
-        
-        Serial.print("here");
-        Serial.print("\t");
         
         int sensors = 100*digitalRead(Bottom_Right) + 10*digitalRead(Bottom_Centre) + digitalRead(Bottom_Left);
         Serial.print(sensors);
@@ -111,6 +132,9 @@ void detection_ISR(){
         if (sensors == 10) { // hole
           dc.stop();
           cone_deploy = true; 
+          Serial.print(HC_q.store[0]);
+          r=enq(&HC_q,Hole); // Global variable queue - hole or crack queue - enqueue(Hole)
+          Serial.print(HC_q.store[0]);
           hole = YES; 
           hole_or_crack = Hole; 
           Serial.print("Hole");
@@ -120,6 +144,7 @@ void detection_ISR(){
           crack_position = RIGHT;
           cone_deploy = true; 
           right_crack = YES;
+          r=enq(&HC_q,Crack); // Global variable queue - hole or crack queue - enqueue(Crack)
           hole_or_crack = Crack;
           Serial.print("Right Crack");
           Serial.print("\t");
@@ -129,13 +154,15 @@ void detection_ISR(){
           cone_deploy = true; 
           left_crack = YES; 
           hole_or_crack = Crack;
+          r=enq(&HC_q, Crack); // Global variable queue - hole or crack queue - enqueue(Crack)
           Serial.print("Left Crack");
           Serial.print("\t");
         } else if (sensors == 111) {  // centre crack
-           dc.stop();
+          dc.stop();
           crack_position = CENTRE;
           cone_deploy = true; 
           hole_or_crack = Crack; 
+          r=enq(&HC_q,Crack); // Global variable queue - hole or crack queue - enqueue(Crack)
           Serial.print("Centre Crack");
           Serial.print("\t");
         }
@@ -153,11 +180,13 @@ void detection_ISR(){
 //Cone Deployment Code//
 void cone_deployment(){
   Serial.print(hole_or_crack);
-  if (hole_or_crack ==  Hole){
+  int rval = 0; 
+  r=deq(&HC_q,&rval);
+  if (rval ==  Hole){
     hole_or_crack = NA; 
     hole_deploy();
   }
-  else if (hole_or_crack == Crack){
+  else if (rval == Crack){
     hole_or_crack = NA; 
     crack_deploy();
   }
@@ -182,7 +211,13 @@ void crack_deploy(){
          stepp.drop_cone();
          servo.displace_left(10.5); //check for setting current_position inside code
          stepp.drop_cone();
-    }else{               //All three sensors triggered
+    } else if (crack_position == CENTRE) {
+         servo.displace_right(5.1);
+         stepp.drop_cone();
+         servo.hard_right();
+         servo.displace_left(5.1);
+         stepp.drop_cone();
+    } else{               //All three sensors triggered
          servo.move_to(4.5); 
          stepp.drop_cone(); 
          servo.displace_right(10.5); //check for setting current_position inside code
@@ -197,7 +232,75 @@ void hole_deploy(){
 //    delay(5000);
     dc.stop();
 //     servo.to_middle()
-    servo.move_to(9.5);
+    //servo.move_to(9.5);
     stepp.drop_cone();
     return;
+}
+
+//Queue Code//
+int init(Queue *x)
+{
+    if (x == NULL)
+    {
+        return -1;
+    }
+    x->max_size = 4;
+    x->size = 0;
+    x->start = 0;
+    return 0;
+}
+
+int enq(Queue *x, int val)
+{
+    if (x == NULL)
+    {
+        return -1;
+    }
+    if (x->size == x->max_size)
+    {
+        /*blew the memory*/
+        return -1;
+    }
+    x->store[(x->start + x->size) % x->max_size] = val;
+    x->size += 1;
+    return 0;
+}
+
+/*pval is a return value*/
+int deq(Queue *x, int *pval)
+{
+    int i=0;
+    if (x == NULL)
+    {
+        return -1;
+    }
+    if (pval == NULL)
+    {
+        return -1;
+    }
+    if (x->size == 0)
+    {
+        return -1;
+    }
+    *pval = x->store[x->start];
+    x->store[x->start] = 0;
+    x->start = (x->start + 1) % x->max_size;
+    x->size -= 1;
+    return 0;
+}
+
+int isEmpty(Queue x)
+{
+    if (r == NULL)
+    {
+        return -1;
+    }
+    if (x.size == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
