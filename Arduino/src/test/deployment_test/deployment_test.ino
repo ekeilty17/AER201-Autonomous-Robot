ba814e65d5d91a42dbf692adb9430ed0e6ecbfba
Standard <Standard@Erics-MacBook-Air-433.local>
2019-03-16 15:29:08 -0400

    Add successive deployment logic

--- Arduino/src/test/deployment_test/deployment_test.ino
+++ Arduino/src/test/deployment_test/deployment_test.ino
@@ -16,8 +16,12 @@
 #define CENTER_CRACK      111
 
 //Global variables for primary initializing 
-volatile int current_position = 0;
+volatile int curr_pos = 0;
 volatile int number_of_cones_deployed = 0;
+
+//Detection ISR Variables 
+volatile int last_obstruction[2] = {0, 0};
+volatile bool isPreviouslyDeployed = false;
 
 //Cone Deployment Functions
 void cone_deployment();
@@ -27,21 +31,12 @@ void right_crack_deploy();
 void centre_crack_deploy();
 void center_crack_deploy();
 
-//Detection ISR Variables 
-volatile int last_obstruction = 0;
-volatile float holes_distance[100];
-volatile float cracks_distance[100];
-volatile int holes_n = 0;
-volatile int cracks_n = 0;
-volatile int crack_position = NULL;
-
 // ISR
 void detection_ISR();
 
 //Initialize Queues
 Queue HC_q(10);
-Queue HC_Dist_q(); 
-Queue C_Pos_q(); 
+Queue HC_Dist_q(10);
 
 //Initialize DC Motors
 // AerDCMotors(int pinL1, int pinL2, int pinR1, int pinR2)
@@ -75,16 +70,25 @@ void setup() {
 
 void loop() {
   while (1){
+
+    // For testing purposes
+    curr_pos += 5;
+    
     Serial.print(HC_q.isEmpty());
     delay(1000);
+    
     if (HC_q.isEmpty() == false){
+      
       Serial.print("\n");
       Serial.print("begin deployment");
       Serial.print("\n");
+      
       cone_deployment();
+      
       Serial.print("done deployment");
       Serial.print("\n");
       Serial.print("\n");
+      
     }
   }
 }
@@ -102,19 +106,24 @@ void detection_ISR(){
         
         int obstruction = 100*digitalRead(SENSOR_RIGHT) + 10*digitalRead(SENSOR_CENTRE) + digitalRead(SENSOR_LEFT);
         HC_q.enq(obstruction);
+        HC_Dist_q.enq(curr_pos);
 
         Serial.print("Detected: ");
         Serial.print(obstruction);
+        Serial.print(" at location ");
+        Serial.print(curr_pos);
         Serial.print("\t");
-        int *q = HC_q.getQueue();
-        for (int i=0; i<HC_q.getSize(); i++) {
-          Serial.print(q[i]);
-          Serial.print(" ");
-        }
-        Serial.print("\t");
-        Serial.print("'HC_q.isEmpty()' = ");
-        Serial.print(HC_q.isEmpty());
-        Serial.print("\n");
+
+        // Printing Queue
+//        int *q = HC_q.getQueue();
+//        for (int i=0; i<HC_q.getSize(); i++) {
+//          Serial.print(q[i]);
+//          Serial.print(" ");
+//        }
+//        Serial.print("\t");
+//        Serial.print("'HC_q.isEmpty()' = ");
+//        Serial.print(HC_q.isEmpty());
+//        Serial.print("\n");
     }
 
     lastInterrupt = millis();
@@ -131,27 +140,70 @@ void cone_deployment(){
   dc.stop();
 
   int obstruction = HC_q.deq();
-  Serial.print("Deploying obstruction: ");
-  Serial.print(obstruction);
-  Serial.print("\n");
-  
-  switch(obstruction) {
-    case HOLE:
-      hole_deploy();
-      break;
-    case LEFT_CRACK:
-      left_crack_deploy();
-      break;
-    case RIGHT_CRACK:
-      right_crack_deploy();
-      break;
-    case CENTRE_CRACK:
-      centre_crack_deploy();
-      break;
-    default:
-      break;
+  int pos = HC_Dist_q.deq();
+  bool shouldCurrentlyDeploy = shouldDeploy(last_obstruction[0], obstruction, pos-last_obstruction[1]);
+
+  if (!isPreviouslyDeployed or (isPreviouslyDeployed and shouldCurrentlyDeploy)) {
+    Serial.print("Deploying obstruction: ");
+    Serial.print(obstruction);
+    Serial.print("\n");
+    
+    switch(obstruction) {
+      case HOLE:
+        hole_deploy();
+        break;
+      case LEFT_CRACK:
+        left_crack_deploy();
+        break;
+      case RIGHT_CRACK:
+        right_crack_deploy();
+        break;
+      case CENTRE_CRACK:
+        centre_crack_deploy();
+        break;
+      default:
+        break;
+    }
   }
+  
+  last_obstruction[0] = obstruction;
+  last_obstruction[1] = pos;
+  isPreviouslyDeployed = (!isPreviouslyDeployed or (isPreviouslyDeployed and shouldCurrentlyDeploy));
+  
   return;
+}
+
+bool shouldDeploy(int prev, int curr, float diff) {
+
+  if (diff > 20.0) {
+    // hole, then crack, >20cm  or
+    // crack, then hole, >20cm
+    return true;
+  }
+  
+  if (prev == HOLE) {
+    if (curr == HOLE) {
+      if (diff < 15.0) {
+        // hole, then hole, <15cm
+        return false;
+      }
+      // hole, then hole, >15cm
+      return true;
+    }
+    // hole, then crack <20cm
+    return false;
+  } else { /*prev == CRACK*/
+    if (curr != HOLE) { /*curr == CRACK*/
+      if (diff < 10) {
+        // crack, then crack <10cm
+        return false;
+      }
+      // crack, then crack, >10cm
+      return true;
+    }
+    // crack, then hole, <20cm
+    return false;
+  }
 }
 
 void hole_deploy() {
